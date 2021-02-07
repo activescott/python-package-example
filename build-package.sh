7098633dcaed7a382c0849f9c77a73af26754bf4
Scott Willeke <scott@willeke.com>
2021-02-06 16:44:33 -0800

    docs: readme updates for eggs

--- build-package.sh
+++ build-package.sh
@@ -6,7 +6,7 @@ die () {
 
 pushd .
 cd ./package-project/src
-# Warning: eggs are deprecated in favor of wheels, and not supported by pip. - https://setuptools.readthedocs.io/en/latest/setuptools.html#bdist-egg-create-a-python-egg-for-the-project
+# Warning: eggs are deprecated in favor of wheels, and not supported by pip. - https://packaging.python.org/discussions/wheel-vs-egg/
 # python3 ./setup.py bdist_egg || die 'python setup failed'
 python3 ./setup.py bdist_wheel || die 'python setup failed'
 popd .
